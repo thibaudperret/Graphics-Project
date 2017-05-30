df6d52bea02512a9cba8ebd52721e9a8e585f07f
valrigori <valentin.rigori@epfl.ch>
2017-05-30 16:19:09 +0200

    clean ray

--- src/se/graphics/proj/Ray.java
+++ src/se/graphics/proj/Ray.java
@@ -32,13 +32,12 @@ public class Ray {
     public static Ray generateRandomRay(Vector3 position, Vector3 normal){
         float thetaN = (float) Math.atan2(normal.y(), normal.x());
         float phiN = (float) Math.acos(normal.z());
-
-//        float theta = thetaN + (float) (Math.random() * Math.PI);
         float theta = thetaN + (float) (((Math.random() * 2) - 1) * Math.PI / 2);
         float phi = phiN + (float) (((Math.random() * 2) - 1) * Math.PI / 2);
          
         return generateRay(position, normal, phi, theta);
     }
+    
     public static Pair<Float, Float> generateSphericalRandomDirection(Vector3 normal) {
         float thetaN = (float) Math.atan2(normal.y(), normal.x());
         float phiN = (float) Math.acos(normal.z());
@@ -60,21 +59,21 @@ public class Ray {
         return new Ray(collisionPoint, newDir);
     }
     
-//    public static Ray refractedRay(Vector3 direction, Vector3 normal, Vector3 collisionPoint, Medium oldMedium, Medium newMedium) {
-//        float theta1 = (float) Math.acos(Math.abs(direction.dot(normal)) / (direction.size() * normal.size()));
-//        float theta2 = (float) Math.asin(Math.sin(theta1) * oldMedium.refractiveIndex() / newMedium.refractiveIndex());
-//        
-//        Vector3 xp = direction.minus(normal.times(direction.dot(normal)));
-//        float b = - direction.size() * (float) Math.cos(theta1);
-//        
-//        Vector3 newDirection = xp.times((float) Math.cos(1 - theta2)).plus(normal.times(b * (float) Math.cos(theta2))).plus(direction.minus(xp).minus(normal.times(b)));
-//        
-//        return new Ray(collisionPoint, newDirection);
-//    }
+/*    public static Ray refractedRay(Vector3 direction, Vector3 normal, Vector3 collisionPoint, Medium oldMedium, Medium newMedium) {
+        float theta1 = (float) Math.acos(Math.abs(direction.dot(normal)) / (direction.size() * normal.size()));
+        float theta2 = (float) Math.asin(Math.sin(theta1) * oldMedium.refractiveIndex() / newMedium.refractiveIndex());
+        
+        Vector3 xp = direction.minus(normal.times(direction.dot(normal)));
+        float b = - direction.size() * (float) Math.cos(theta1);
+        
+        Vector3 newDirection = xp.times((float) Math.cos(1 - theta2)).plus(normal.times(b * (float) Math.cos(theta2))).plus(direction.minus(xp).minus(normal.times(b)));
+        
+        return new Ray(collisionPoint, newDirection);
+    }
+*/
     
     public static Ray refractedRay(Vector3 direction, Vector3 normal, Vector3 collisionPoint, Medium oldMedium, Medium newMedium) {
-        direction = direction.normalise(); // ça fait pas de mal
-        
+        direction = direction.normalise();       
         float ratio = oldMedium.refractiveIndex() / newMedium.refractiveIndex();
         Vector3 nCrossDir = normal.cross(direction);
         Vector3 newDir = (normal.cross(nCrossDir.times(-1f))).times(ratio);
@@ -94,16 +93,10 @@ public class Ray {
     
     public static Vector3 sphericalToCartesianDir(Pair<Float, Float> angles) {
         //TODO check if conversion is valid given our conventions
-
         float theta = angles.getLeft();
         float phi = angles.getRight();
-        
         float sinPhi = (float)Math.sin(theta);
         
-        return new Vector3((float)(sinPhi*Math.cos(theta)),(float)(sinPhi*Math.sin(theta)), (float)Math.cos(phi)).normalise();
-        
+        return new Vector3((float)(sinPhi*Math.cos(theta)),(float)(sinPhi*Math.sin(theta)), (float)Math.cos(phi)).normalise();       
     }
-    
-
-    
-   }
+}
